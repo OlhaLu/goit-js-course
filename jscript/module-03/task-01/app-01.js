1620cafc210e6f9b2ef95b67d7bc9c6059d2d2aa
OlhaLu <olhalutsyshyna@gmail.com>
2019-04-07 22:30:07 +0300

    Delete alert

--- jscript/module-03/task-01/app-01.js
+++ jscript/module-03/task-01/app-01.js
@@ -33,25 +33,19 @@
 
 let logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
-const isLoginValid = login => {
-  return login.length >= 4 && login.length <= 16;
-};
+const isLoginValid = login => login.length >= 4 && login.length <= 16;
 
-const isLoginUnique = (allLogins, login) => {
-  return !allLogins.includes(login);
-};
+const isLoginUnique = (allLogins, login) => !allLogins.includes(login);
 
 const addLogin = (allLogins, login) => {
   if (!isLoginValid(login)) {
-    alert('Ошибка! Логин должен быть от 4 до 16 символов');
-    return;
+    return 'Ошибка! Логин должен быть от 4 до 16 символов';
   }
   if (!isLoginUnique(allLogins, login)) {
-    alert('Такой логин уже используется!');
-    return;
+    return 'Такой логин уже используется!';
   }
   allLogins.push(login);
-  alert('Логин успешно добавлен!');
+  return 'Логин успешно добавлен!';
 };
 
 // Вызовы функции для проверки
