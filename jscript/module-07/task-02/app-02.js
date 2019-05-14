a16fb94169e2f9a29331984027e70458f8d58aa7
OlhaLu <olhalutsyshyna@gmail.com>
2019-05-14 21:20:36 +0300

    Add task 02-2.1

--- jscript/module-07/task-02/app-02.js
+++ jscript/module-07/task-02/app-02.js
@@ -96,7 +96,7 @@ const getUniqueSkills = users =>
   users.reduce((allSkill, user) => {
     allSkill.push(...user.skills);
     return allSkill
-      .filter((item, pos, allSkill) => {
+      .filter((item, pos) => {
         return allSkill.indexOf(item) === pos;
       })
       .sort();
@@ -110,5 +110,9 @@ console.log(getUniqueSkills(users));
 // Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
 
 const getNamesSortedByFriendsCount = users =>
-  console.log(getNamesSortedByFriendsCount(users));
+  users
+    .sort((prevEl, nextEl) => prevEl.friends.length - nextEl.friends.length)
+    .map(user => user.name);
+
+console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
