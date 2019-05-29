55c0f7b9ab3ae8486358579eb606cdad6d6a4cd5
OlhaLu <olhalutsyshyna@gmail.com>
2019-05-29 15:08:36 +0300

    Add task 04

---
+++ additional-tasks/module-08/task-04/script-04.js
@@ -0,0 +1,66 @@
+'use strics';
+
+/*
+  Напишите скрипт для создания галлереи изображений. 
+  
+  - На вкладке HTML уже есть ul.gallery.
+  - Используйте массив объектов для создания тегов img вложенных в li
+  - Оформление по вкусу, можно и не делать, достаточно чтобы каждое 
+    изображение было 300px по ширине
+  - Добавьте все элементы галлереи в ul.gallery
+*/
+
+const galleryItems = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/1216482/pexels-photo-1216482.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Two Brown Hen and One Red Rooster',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/1316294/pexels-photo-1316294.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Macaw Birds',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/41178/africa-animal-big-carnivore-41178.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: '2 Lion on Grass Field during Daytime',
+  },
+];
+
+function createGallery(images) {
+  const gallery = document.querySelector('.gallery');
+  images.forEach(image => {
+    gallery.appendChild(createLiElement(image));
+  });
+}
+
+function createLiElement({ url, alt }) {
+  const li = document.createElement('li');
+  const img = document.createElement('img');
+
+  img.setAttribute('src', url);
+  img.setAttribute('alt', alt);
+
+  li.appendChild(img);
+  img.style.width = '300px';
+
+  return li;
+}
+
+createGallery(galleryItems);
